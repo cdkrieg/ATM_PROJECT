8374a3f5e21354e88d3502164894f4c79d43361e
cdkrieg <cdkrieg@outlook.com>
2022-03-10 20:44:00 -0600

    functioning PIN verification

--- atm.js
+++ atm.js
@@ -12,6 +12,17 @@ function deposit(){
 
 }
 
-function validatePin(){
-
+function validatePin(input){
+    let pin = 2029;
+    if(input == pin){
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+module.exports.validatePin = validatePin;
+module.exports.deposit = deposit;
+module.exports.withdraw = withdraw;
+module.exports.getBalance = getBalance;
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 "use strict"
+const prompt = require('prompt-sync')();
+const {validatePin, withdraw, getBalance, deposit} = require('./atm');
+
+function welcome(){
+    console.log("Welcome to the Senior Chief Banking System!");
+    let pin = promptFor('Please enter your PIN to continue: ', validatePin);
+    console.log("")
+}
+
+function atmSelection(){
+    let selection = promptFor("Please select from the following options: \n<1> Check Account Balance \n<2> Withdrawal \n<3> Deposit \n<4> Done", numberValidation);
+    switch (selection){
+        case "1":
+
+            break;
+        case "2":
+
+            break;
+        case "3":
+
+            break;
+        case "4":
+        
+            break;
+        default:
+            break;
+    }
+}
+
+function promptFor(question, valid) {
+    let isValid;
+    do {
+      var response = prompt(question);
+      if (response !== null) {
+        response.trim();
+      }
+      isValid = valid(response);
+    } while (response === "" || isValid === false || response === null);
+    return response;
+  }
+
+  function numberValidation(input){
+      if(parseInt(input) > 0 && parseInt(input) < 5){
+        return true;
+      } else {
+          return false;
+      }
+  }
+
+  welcome();
